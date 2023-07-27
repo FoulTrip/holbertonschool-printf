9b6a30747f7bfdf6bfbc32a6868adb2eccfbe7ab
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-27 10:02:55 -0500

    act006.1

--- helpers.c
+++ helpers.c
@@ -5,14 +5,14 @@
 /**
  * print_char - Print a character to the output
  * @c: The character to be printed
- * 
+ *
  * This function prints the provided character to the standard output.
- * 
+ *
  * @c: The character to be printed.
- * 
+ *
  * Return: Returns 1 if the character was printed successfully.
  */
-int print_char(char c) 
+int print_char(char c)
 {
     putchar(c);
     return (1);
@@ -21,15 +21,15 @@ int print_char(char c)
 /**
  * print_string - Print a string to the output
  * @str: Pointer to the string to be printed
- * 
+ *
  * This function prints the given string to the standard output.
- * It iterates through the characters of the string and prints 
+ * It iterates through the characters of the string and prints
  * each character using putchar().
- * The function continues until it reaches the null-terminating 
+ * The function continues until it reaches the null-terminating
  * character ('\0') of the string.
- * 
+ *
  * @str: Pointer to the null-terminated string to be printed.
- * 
+ *
  * Return: Returns the number of characters printed.
  */
 int print_string(const char *str)
@@ -47,14 +47,14 @@ int print_string(const char *str)
 /**
  * print_int - Prints an integer to the output
  * @num: The integer to be printed
- * 
- * This function prints the provided integer to 
+ *
+ * This function prints the provided integer to
  * the standard output using the printf function.
- * 
+ *
  * @num: The integer to be printed.
- * 
- * Return: Returns the number of characters printed 
- *         (excluding the null byte used to end 
+ *
+ * Return: Returns the number of characters printed
+ *         (excluding the null byte used to end
  *         output to strings).
  */
 int print_int(int num)
@@ -62,7 +62,7 @@ int print_int(int num)
     int is_negative, i;
     char digits[12];
 
-    if (num == 0) 
+    if (num == 0)
     {
         putchar('0');
     }
@@ -73,7 +73,7 @@ int print_int(int num)
         num = -num;
     }
 
-    while (num > 0) 
+    while (num > 0)
     {
         digits[i++] = '0' + (num % 10);
         num /= 10;
@@ -95,25 +95,25 @@ int print_int(int num)
 /**
  * _printf - Print formatted output based on the provided
  *           format string and arguments.
- * @format: A pointer to the format string containing format 
+ * @format: A pointer to the format string containing format
  *          specifiers.
- * 
- * This function prints formatted output to the standard output 
- * based on the provided format string and the corresponding arguments. 
- * The format string may contain special format specifiers starting 
+ *
+ * This function prints formatted output to the standard output
+ * based on the provided format string and the corresponding arguments.
+ * The format string may contain special format specifiers starting
  * with '%', which will be replaced by the corresponding argument values.
- * 
+ *
  * @format: The format string containing format specifiers.
- * 
+ *
  * Return: The total number of characters printed.
  */
 int _printf(const char *format, ...)
 {
     char specifier;
-    int count = 0; 
+    int count = 0;
 
     va_list args;
-    va_start(args, format); 
+    va_start(args, format);
 
     while (*format)
     {
@@ -122,38 +122,34 @@ int _printf(const char *format, ...)
             format++;
             specifier = *format;
 
-            switch (specifier)
-            {
-            case 'c':
+            if (specifier == 'c')
             {
                 char c = va_arg(args, int);
                 count += print_char(c);
                 break;
             }
-            case 's':
+            else if (specifier == 's')
             {
                 char *str = va_arg(args, char *);
                 count += print_string(str);
                 break;
             }
-            case '%':
+            else if (specifier == '%')
             {
                 count += print_char('%');
                 break;
             }
-            case 'i':
-            case 'd':
+            else if (specifier == 'i' || specifier == 'd')
             {
                 int num = va_arg(args, int);
                 count += print_int(num);
                 break;
             }
-            default:
+            else
             {
                 count += print_char('%');
                 count += print_char(*format);
                 break;
-            }
             }
         }
         else
