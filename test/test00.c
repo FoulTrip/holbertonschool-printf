41bbffec2eaa45af2241d135e333c689bb6881c0
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-27 12:27:58 -0500

    act007

---
+++ test/test00.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void) {
+    int num1 = 5;
+    int num2 = 9;
+    int result = num1 + num2;
+
+    _printf("The sum of the numbers (%i) and (%i) is: %i", num1, num2, result);
+}
