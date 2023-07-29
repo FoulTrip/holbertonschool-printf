0e712b7d2d0fbeb8ce7b9930aa59bc94cd533266
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-29 09:36:06 -0500

    act001

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <limits.h>
-#include <stdint.h>
 
 /**
  * main - The main function of the program.
@@ -13,12 +12,10 @@ int main(void)
 	int len;
         int len2;
         unsigned int ui;
-        uintptr_t *addr;
 
         len = _printf("Let's try to printf a simple sentence.\n");
         len2 = printf("Let's try to printf a simple sentence.\n");
         ui = (unsigned int)INT_MAX + 1024;
-        addr = (void *)0x7ffe637541f0;
         _printf("Length:[%d, %i]\n", len, len);
         printf("Length:[%d, %i]\n", len2, len2);
         _printf("Negative:[%d]\n", -762534);
